d81c25f3b498772723a0be3768d95f0c6bf922f5
Yamini Gosula <yaminisurekha2002@gmail.com>
2024-10-18 12:03:30 +0530

    New commits

---
+++ HTML-Jquery/js/FormValidationUsingPlugin.js
@@ -0,0 +1,109 @@
+$(document).ready(function(){
+ 
+    $("#form").validate({
+
+        rules:{
+
+            firstName:{
+
+                required : true
+            },
+            lastName:{
+                required : true,
+                minlength : 5
+            },
+            email: {
+
+                required : true,
+                email:true
+            },
+            phone: {
+                required : true,
+                indianPhone : true,
+                minlength : 10,
+                maxlength : 10,
+                digits : true
+            },
+            address: {
+                required : true
+            },
+            city: {
+
+                required : true
+
+            },
+
+            createPassword : {
+
+                required : true,
+                minlength : 8
+
+            },
+
+            repeatPassword:{
+
+                required : true,
+                equalTo : "#createPassword"
+            },
+
+            state: {
+                required : true
+            }
+        },
+        messages : {
+
+            
+            firstName : {
+
+                required : "First Name is required"
+                
+            },
+            lastName : {
+                required : "Last Name is required"
+            },
+            email:{
+                    required: "Email is required",
+                    email : "Invalid Email,Please enter valid format"
+            },
+            phone:{
+                required : "Phone number is required",
+                minlength : "Phone must contain 10 digits",
+                maxlength : "phone number should not exceed 10 digits",
+                digits : "phone number should contain only digits"
+            },
+            address: {
+                required :"Address is required"
+            },
+            city : {
+                required : "City is required"
+            },
+
+            createPassword : {
+                    required : "Password field is required",
+                    minlength : "Password must contain minimum of 8 characters"
+            },
+            repeatPassword : {
+                required : "Re-enter password",
+                equalTo : "Wrong password,Re-enter the password"
+            },
+
+            state : {
+                required : "please select your state"
+            },
+            
+        },
+
+        errorPlacement : function(error,element){
+                error.insertAfter(element.closest(".input-group"));
+        },
+        success: function(label,element){
+                $(element).closest(".input-group").addClass('success');
+                $(element).closest(".input-group").removeClass('error');
+        },
+
+        
+
+
+    });
+
+});
--- HTML-Jquery/js/formValidation.js
+++ HTML-Jquery/js/formValidation.js
@@ -1,139 +1,151 @@
-/*const form = document.getElementById('form');
-const firstName = document.getElementById('firstName');
-const lastName=document.getElementById('lastName');
-const email = document.getElementById('email');
-const phone = document.getElementById('phone');
-const address = document.getElementById('address');
-const city = document.getElementById('city');
-const password = document.getElementById('createPassword');
-const Rpassword = document.getElementById('repeatPassword');
-const state = document.getElementById('state');*/
+
+$(document).ready(function(){
+
+
+    const form=$('#form');
+
+
+    form.on('submit',function(e){
+    
+       e.preventDefault();
+       validateInputs();
+     })
+    
+    
+    
+    function validateInputs(){
+        console.log("calling validateInputs");
+    
+        const firstNameVal =$('#firstName').val().trim();
+        const lastNameVal = $('#lastName').val().trim();
+        const emailVal  = $('#email').val().trim();
+        const phoneVal = $('#phone').val().trim();
+        const addressVal = $('#address').val().trim();
+        const cityVal = $('#city').val().trim();
+        const passwordVal = $('#createPassword').val().trim();
+        const RpasswordVal = $('#repeatPassword').val().trim();
+        const stateVal = $('#state').val().trim();
+    
+    
+        if(firstNameVal===''){
+            setError($('#firstName'),'FirstName is required');
+        }
+        else{
+            setSuccess($('#firstName'));
+        }
+    
+        if(lastNameVal===''){
+            setError($('#lastName'),'LastName is required');
+        }
+        else if(lastNameVal.length>5){
+            setError($('#lastName'),'LastName should be less than 5');
+        }
+        else{
+            setSuccess($('#lastName'));
+        }
+    
+        if(emailVal===''){
+            setError($('#email'),'Email Cannot be Empty');
+        }
+        else if(!isValidEmail(emailVal)){
+    
+            setError($('#email'),'Not a Valid Email');
+        }
+        else{
+            setSuccess($('#email'));
+        }
+    
+
+        
+        const format = /^\d{10}$/;
+        if(phoneVal===''){
+            setError(phone,'phone is required');
+        }
+        else if(!format.test(phoneVal)){
+            setError(phone,'phone number should be in digits');
+        }
+        else{
+            setSuccess(phone);
+        }
 
 
 
-console.log(form);
-
-
-form.addEventListener('submit',(e)=>{
-
-   e.preventDefault();
-   validateInputs();
- })
-
-
-
-function validateInputs(){
-    console.log("calling validateInputs");
-
-    const firstNameVal =$('#firstName').value().trim();
-    const lastNameVal = $('#lastName').value().trim();
-    const emailVal  = $('#email').value().trim();
-    const phoneVal = $('#phone').value().trim();
-    const addressVal = $('#address').value().trim();
-    const cityVal = $('#city').value().trim();
-    const passwordVal = $('#password').value().trim();
-    const RpasswordVal = $('#Rpassword').value().trim();
-    const stateVal = $('#state').value().trim();
-
-
-    if(firstNameVal===''){
-        setError($('#firstName'),'FirstName is required');
+    
+        if(addressVal===''){
+            setError($('#address'),'Address is required');
+        }
+        else{
+            setSuccess($('#address'));
+        }
+    
+        if(cityVal===''){
+            setError($('#city'),'city is required');
+        }
+        else{
+            setSuccess($('#city'));
+        }
+    
+        if(passwordVal===''){
+            setError($('#createPassword'),'password is required');
+        }
+        else if(passwordVal.length<8){
+            setError($('#createPassword'),'password must contain atleate 8 characters');
+        }
+        else{
+            setSuccess($('#createPassword'));
+        }
+    
+        if(RpasswordVal===''){
+            setError($('#repeatPassword'),'confirm your password');
+        }
+        else if(RpasswordVal!==passwordVal){
+            setError($('#repeatPassword'),'password do not match');
+        }
+        else{
+            setSuccess($('#repeatPassword'));
+        }
+    
+    
+        if (stateVal === 'Please Select your state') {
+            setError($('#state'), 'State selection is required');
+        } else {
+            setSuccess($('#state'));
+        }
+    
     }
-    else{
-        setSuccess(firstName);
+    
+    function setError(element,message){
+        const inputGroup = element.parent();
+        const errorElement = inputGroup.find('.error');
+        errorElement.text(message);
+        inputGroup.addClass('error').removeClass('success');
     }
-
-    if(lastNameVal===''){
-        setError($('#lastName'),'LastName is required');
+    
+    
+    function setSuccess(element){
+        const inputGroup = element.parent();
+        const errorElement = inputGroup.find('.error');
+        errorElement.text('');
+        inputGroup.addClass('success').removeClass('error');
     }
-    else{
-        setSuccess(lastName);
+    
+    
+    
+    function isValidEmail(email){
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
     }
-
-    if(emailVal===''){
-        setError($('#email'),'Email Cannot be Empty');
-    }
-    else if(!isValidEmail(emailVal)){
-
-        setError($('#email'),'Not a Valid Email');
-    }
-    else{
-        setSuccess($('#email'));
-    }
-
-    if(phoneVal===''){
-        setError($('#phone'),'phone is required');
-    }
-    else if(phoneVal.length<10){
-        setError($('#phone'),'phone number must contain 10 digits');
-    }
-    else{
-        setSuccess($('#phone'));
-    }
-
-    if(addressVal===''){
-        setError($('#address'),'Address is required');
-    }
-    else{
-        setSuccess($('#address'));
-    }
-
-    if(cityVal===''){
-        setError($('#city'),'city is required');
-    }
-    else{
-        setSuccess($('#city'));
-    }
-
-    if(passwordVal===''){
-        setError($('#password'),'password is required');
-    }
-    else if(passwordVal.length<8){
-        setError($('#password'),'password must contain atleate 8 characters');
-    }
-    else{
-        setSuccess($('#password'));
-    }
-
-    if(RpasswordVal===''){
-        setError($('#Rpassword'),'Rpassword is required');
-    }
-    else if(RpasswordVal!==passwordVal){
-        setError($('#Rpassword'),'password do not match');
-    }
-    else{
-        setSuccess($('#Rpassword'));
-    }
-
-
-    if (stateVal === 'Please Select your state') {
-        setError($('#state'), 'State selection is required');
-    } else {
-        setSuccess($('#state'));
-    }
-
-}
-
-function setError(element,message){
-    const inputGroup = element.parentElement;
-    const errorElement = inputGroup.querySelector('.error');
-    errorElement.innerText = message;
-    inputGroup.classList.add('error')
-    inputGroup.classList.remove('success')
-}
-
-
-function setSuccess(element){
-    const inputGroup = element.parentElement;
-    const errorElement = inputGroup.querySelector('.error');
-    errorElement.innerText = '';
-    inputGroup.classList.add('success')
-    inputGroup.classList.remove('error')
-}
+    
+    
 
 
 
-function isValidEmail(email){
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
 
+
+
+
+
+});
+
+
+
+   
